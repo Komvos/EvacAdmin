a7401bd00989b86662f0fefacbfa0a805600332b
Komvos <tasos@komvos.gr>
2025-03-26 17:16:24 +0200

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'evacadmin',
+  webDir: 'www'
+};
+
+export default config;
